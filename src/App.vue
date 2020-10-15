2227e1e6608db9a5faceec77c351481852a95dd1
Hanna Edlund <hanna@edlunds.biz>
2020-10-15 22:14:51 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,111 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="#D1C2BF"
+      clipped-left
+      flat
+    >
+      <v-card width="30vh" dark class="pt-14" color="#2A2A2A">
+        <v-card-text align="center">Loggan ska vara här</v-card-text>
+      </v-card>
+      <v-spacer></v-spacer>
+
+      <v-tabs>
+        <v-tab>
+          Start
+        </v-tab>
+        <v-tab target="_blank">
+          Start
+        </v-tab>
+        <v-tab target="_blank" color="rgb(255,255,255,0.3)">
+          Start
+        </v-tab>
+        <v-tab target="_blank" color="rgb(255,255,255,0.3)">
+          Start
+        </v-tab>
+        <v-tab target="_blank" color="rgb(255,255,255,0.3)">
+          Start
+        </v-tab>
+        <v-tab target="_blank" color="rgb(255,255,255,0.3)">
+          Start
+        </v-tab>
+      </v-tabs>
+
+<!--      <v-btn target="_blank" color="rgb(255,255,255,0.3)">Produkter</v-btn>-->
+<!--      <v-spacer></v-spacer>-->
+
+<!--      <v-btn target="_blank" color="rgb(255,255,255,0.3)">Återförsäljare</v-btn>-->
+<!--      <v-spacer></v-spacer>-->
+
+<!--      <v-btn target="_blank" color="rgb(255,255,255,0.3)">Om oss</v-btn>-->
+<!--      <v-spacer></v-spacer>-->
+
+<!--      <v-btn target="_blank" color="rgb(255,255,255,0.3)">Kontakt</v-btn>-->
+<!--      <v-spacer></v-spacer>-->
+
+<!--      <v-btn target="_blank" color="rgb(255,255,255,0.3)">Hjälp</v-btn>-->
+<!--      <v-spacer></v-spacer>-->
+
+    </v-app-bar>
+
+    <v-navigation-drawer
+        permanent
+        app
+        clipped
+    >
+      <v-list>
+        <v-list-item class="pt-4">
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              Hanna Edlund
+            </v-list-item-title>
+            <v-list-item-subtitle>hanna@edlunds.biz</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+      <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh sed pulvinar proin gravida. Risus nullam eget felis eget. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat in. Fermentum leo vel orci porta non pulvinar neque laoreet suspendisse.</v-card-text>
+      <v-card-text>Ac turpis egestas maecenas pharetra convallis posuere morbi leo urna. Est lorem ipsum dolor sit amet consectetur. Dui vivamus arcu felis bibendum ut tristique et. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl.</v-card-text>
+
+      <v-list
+          nav
+          dense
+      >
+        <v-list-item link href="https://github.com/Meriono" target="_blank">
+          <v-list-item-icon>
+            <v-icon>mdi-github</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>My GitHub</v-list-item-title>
+        </v-list-item>
+        <v-list-item link href="https://previews.dropbox.com/p/thumb/AA_SAkBY8xnT87eXLTE815-rg8UiDu6x2V9BsmDYYtHk4v7r9jcVmcMS6xNBrPL3tPKa21EYYcoli6aGP3G0hXKE8nXankxidMGi2jouFg1lT_GTI8RdREghcRv6J4lCGf_6VsSdfePZhdRdwWBu10Mob_VN65-_8l2M-Dnr803UoEjsZkpj77PWB9ijjCUHJAQdtsdXx837BaldWC7tVKmelLj5P7yu5cHVov-1QNRX41gS_kOMS3oJaleP2rTs1e2LPqVmAkVIoetFe9FpQjg36ieFJNmV95Fxm8lU25KuIsmjTYLGiw7BlIf9L0wqsm_IwShZQZliJZn8avXHYRVi/p.jpeg?fv_content=true&size_mode=5"  target="_blank">
+          <v-list-item-icon>
+            <v-icon>mdi-image</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Wireframe</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <start></start>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Start from "@/components/Start";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Start,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Start.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-container>
+    <v-card height="70vh" width="170vh" dark color="#2A2A2A">
+      <v-row>
+        <v-col cols="4">
+          <h1 align="center" class="pink--text">NAMN</h1>
+          <v-card
+              class="overflow-y-auto"
+              flat
+              max-height="60vh"
+              color="transparent"
+          >
+            <v-card-text>
+              <p class="pl-4 text--lighten-4 pink--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh sed pulvinar proin gravida. Risus nullam eget felis eget. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat in.</p>
+              <p class="pl-4 text--lighten-3 pink--text">Fermentum leo vel orci porta non pulvinar neque laoreet suspendisse. Ac turpis egestas maecenas pharetra convallis posuere morbi leo urna. Est lorem ipsum dolor sit amet consectetur. Dui vivamus arcu felis bibendum ut tristique et. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl.</p>
+              <p class="pl-4 text--lighten-4 pink--text">Odio ut enim blandit volutpat maecenas. Hac habitasse platea dictumst quisque sagittis purus sit. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. Posuere urna nec tincidunt praesent semper feugiat nibh. Sociis natoque penatibus et magnis dis.</p>
+              <p class="pl-4 text--lighten-3 pink--text">Dolor morbi non arcu risus quis varius. Eget mi proin sed libero enim sed faucibus turpis. Id faucibus nisl tincidunt eget nullam non. Nunc sed velit dignissim sodales ut eu sem integer vitae.</p>
+            </v-card-text>
+          </v-card>
+
+        </v-col>
+        <v-col cols="8">
+          <v-card height="100%" max-height="66.5vh" width="98%" color="grey" flat>
+            <v-row>
+              <v-col class="px-8 pt-4">
+                <v-img class="rounded"
+                    :src="`https://picsum.photos/700/460?image=${3 * 5 + 10}`"
+                >
+                </v-img>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
+
+
+
+    <v-card height="10vh" width="170vh" color="#D1C2BF" class="my-5">
+
+      <v-row>
+          <v-card
+              class="overflow-y-auto my-2"
+              flat
+              max-width="154vh"
+              max-height="8vh"
+              color="transparent"
+          >
+            <v-card-text>
+              <p class="text--black">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh sed pulvinar proin gravida. Risus nullam eget felis eget. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat in. Fermentum leo vel orci porta non pulvinar neque laoreet suspendisse. Ac turpis egestas maecenas pharetra convallis posuere morbi leo urna. Est lorem ipsum dolor sit amet consectetur. Dui vivamus arcu felis bibendum ut tristique et. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl.</p>
+            </v-card-text>
+          </v-card>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Start",
+
+  data() {
+    return {
+      loading: false,
+      selection: 1,
+      products: []
+    }
+  },
+
+  methods: {
+    namn() {
+      this.random = "Oj nu ändrade vi" + this.nummer
+      this.nummer++
+    },
+    addproduct() {
+      this.products.push({
+        namn:"En produkt",
+        kostnad: 299,
+        typ: "sko"
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+::-webkit-scrollbar {
+  width: 3px;
+}
+
+::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+  border-radius: 10px;
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5);
+}
+</style>
